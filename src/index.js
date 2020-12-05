3a77a210fc1b203997b57a945471338950041818
vera douye <veralloydekpebu@gmail.com>
2020-12-05 08:59:33 +0100

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,6 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import TodoContainer from "./components/TodoContainer";
+import "./app.css";
+
+ReactDOM.render(<TodoContainer />, document.getElementById("root"))
